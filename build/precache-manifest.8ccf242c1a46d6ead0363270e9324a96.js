1f65c1485bc31d316a84ab46ae42e276143a8623
LongKC <longkc@itsol.vn>
2023-03-09 15:49:15 +0700

    dev

---
+++ build/precache-manifest.8ccf242c1a46d6ead0363270e9324a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc636c7d4d6c54920a78c5bddbe38f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "606d5e673c1d8143b78a",
+    "url": "/static/css/main.270bfac1.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606d5e673c1d8143b78a",
+    "url": "/static/js/main.04c91d73.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
